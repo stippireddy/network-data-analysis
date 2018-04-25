73128bf1067dab97939cf7d0ebc40996c544c85e
stippireddy <stippireddy@AT-L-M-200-B02.ad.ufl.edu>
2018-04-25 09:33:32 -0400

    Saving work

--- src/BloomFilter.java
+++ src/BloomFilter.java
@@ -1,6 +1,30 @@
+import java.util.Random;
 
 public class BloomFilter {
+
+	private int size;
+	private int numberOfHashes;
+	private int[] randomNumbers;
+
+	public BloomFilter(int size, int numberOfHashes) {
+		this.size = size;
+		this.numberOfHashes = numberOfHashes;
+		randomNumbers = new int[numberOfHashes];
+		Random r = new Random(System.currentTimeMillis());
+		for (int i = 0; i < numberOfHashes; i++) {
+			randomNumbers[i] = r.nextInt(Integer.MAX_VALUE);
+		}
+	}
+
+	public void encodeElement(String element) {
+
+	}
+
+	public boolean isElementExists(String element) {
+		return false;
+	}
+
 	public static void main(String[] args) {
-		
+
 	}
 }
